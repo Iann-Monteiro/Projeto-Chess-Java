2e328543c00bba096ecc8a4f64a826ae4a21c9b1
Iann Monteiro <iann.monteiro@softfocus.com.br>
2024-03-12 16:06:23 -0300

    term

---
+++ src/Chess/ChessException.java
@@ -0,0 +1,9 @@
+package Chess;
+
+public class ChessException extends Exception{
+
+    public void ChessException(String msg){
+
+    }
+
+}
---
+++ src/Chess/ChessMatch.java
@@ -0,0 +1,17 @@
+package Chess;
+import boardgame.Board;
+
+public class ChessMatch {
+
+
+    private Board board;
+    private int turn;
+    private Color currentPlayer;
+    private boolean check;
+    private boolean checkMate;
+    private ChessPiece enPassantVulnerable;
+    private ChessPiece promoted;
+
+    public ChessMatch = new Board(8, 8);
+
+}
